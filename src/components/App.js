dffde796e436ad105232386915a868852d0cb3d4
Giovanni Comparato <gcpal4@gmail.com>
2021-02-23 16:00:24 +0100

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,118 @@
-import '../css/App.css';
-import {useState, useEffect} from 'react'
-import { AddAppointments } from './AddAppointments';
-import { ListAppointments } from './ListAppointments';
-import { SearchAppointments } from './SearchAppointments';
-import {without} from 'lodash'
-
+import "../css/App.css";
+import { useState, useEffect } from "react";
+import { AddAppointments } from "./AddAppointments";
+import { ListAppointments } from "./ListAppointments";
+import { SearchAppointments } from "./SearchAppointments";
+import { without, findIndex } from "lodash";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [formDisplay, setDisplay] = useState(false);
+  const [orderBy, setOrderBy] = useState("petName");
+  const [orderDir, setOrderDir] = useState("asc");
+  const [query, setQuery] = useState("");
 
-const [data, setData] = useState(null)
-const [formDisplay, setDisplay] = useState(false)
+  useEffect(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) =>
+        setData(
+          data.map((item, i) => {
+            return (item = {
+              ...item,
+              aptId: i,
+            });
+          })
+        )
+      );
+  }, []);
+  // console.log(data);
 
+  const searchApts = (query) => {
+    setQuery(query);
+  };
 
-useEffect(()=>{
-  fetch('./data.json')
-  .then(response => response.json())
-  .then(setData)
-  
-  
-}, [])
-// console.log(data)
+  const addAppointment = (apt) => {
+    let tempApts = data;
+    tempApts.unshift(apt);
+    setData(tempApts);
+  };
 
-const deleteApt = (apt)=>{
-  let tempApts = data
-  tempApts = without(tempApts, apt)
-  setData(tempApts) 
-}
+  const deleteApt = (apt) => {
+    let tempApts = data;
+    tempApts = without(tempApts, apt);
+    setData(tempApts);
+  };
 
-const toggleDisplay = ()=>{
-  setDisplay(!formDisplay)
-}
+  const toggleDisplay = () => {
+    setDisplay(!formDisplay);
+  };
 
+  const changeOrder = (order, dir) => {
+    setOrderBy(order);
+    setOrderDir(dir);
+  };
 
-if(!data) return null
+  const updateInfo = (name, value, id) => {
+    let tempApts = data;
+    let index = findIndex(data, { aptId: id });
+    tempApts[index][name] = value;
+    setData(tempApts);
+  };
+
+  if (!data) return null;
+
+  let order;
+  let filteredApts = data;
+  if (orderDir === "asc") {
+    order = 1;
+  } else {
+    order = -1;
+  }
+
+  filteredApts = filteredApts
+    .sort((a, b) => {
+      if (a[orderBy].toLowerCase() < b[orderBy].toLowerCase()) {
+        return -1 * order;
+      } else {
+        return 1 * order;
+      }
+    })
+    .filter((item) => {
+      return (
+        item["petName"].toLowerCase().includes(query.toLowerCase()) ||
+        item["ownerName"].toLowerCase().includes(query.toLowerCase()) ||
+        item["aptNotes"].toLowerCase().includes(query.toLowerCase())
+      );
+    });
 
   return (
-
     <main className="page bg-white" id="petratings">
       <div className="container">
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              <AddAppointments 
-              formDisplay={formDisplay}
-              toggleDisplay={toggleDisplay}
+              <AddAppointments
+                formDisplay={formDisplay}
+                toggleDisplay={toggleDisplay}
+                addAppointment={addAppointment}
               />
-              <SearchAppointments />
-              <ListAppointments 
-              appointments={data}
-              deleteApt={deleteApt}
+              <SearchAppointments
+                orderBy={orderBy}
+                orderDir={orderDir}
+                changeOrder={changeOrder}
+                searchApts={searchApts}
+              />
+              <ListAppointments
+                appointments={filteredApts}
+                deleteApt={deleteApt}
+                updateInfo={updateInfo}
               />
             </div>
           </div>
         </div>
       </div>
     </main>
-
   );
 }
 
--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,38 +1,66 @@
-import {FaTimes} from 'react-icons/fa'
-import Moment from 'react-moment'
+import { FaTimes } from "react-icons/fa";
+import Moment from "react-moment";
 
-export function ListAppointments(props){
+export function ListAppointments(props) {
+  return (
+    <div className="appointment-list item-list mb-3">
+      {props.appointments.map((item) => (
+        <div className="pet-item col media py-3" key={item.aptId}>
+          <div className="mr-3">
+            <button
+              className="pet-delete btn btn-sm btn-danger"
+              onClick={() => props.deleteApt(item)}
+            >
+              <FaTimes />
+            </button>
+          </div>
 
-    return(  
-        <div className="appointment-list item-list mb-3">
-        {props.appointments.map(item => (
-          <div className="pet-item col media py-3" key={props.appointments.indexOf(item)}>
-            <div className="mr-3">
-              <button className="pet-delete btn btn-sm btn-danger" onClick={()=>props.deleteApt(item)}>
-                <FaTimes />
-              </button>
+          <div className="pet-info media-body">
+            <div className="pet-head d-flex">
+              <span
+                className="pet-name"
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={(e) =>
+                  props.updateInfo("petName", e.target.innerText, item.aptId)
+                }
+              >
+                {item.petName}
+              </span>
+              <span className="apt-date ml-auto">
+                <Moment
+                  date={item.aptDate}
+                  parse="YYYY-MM-DD hh:mm"
+                  format="D MMM YYYY - HH:mm"
+                />
+              </span>
             </div>
 
-            <div className="pet-info media-body">
-              <div className="pet-head d-flex">
-                <span className="pet-name">{item.petName}</span>
-                <span className="apt-date ml-auto">
-                <Moment 
-                date={item.aptDate}
-                parse="YYYY-MM-dd hh:mm"
-                format="D MMM YYYY - HH:mm"
-                />
-                </span>
-              </div>
-
-              <div className="owner-name">
-                <span className="label-item">Owner: </span>
-                <span>{item.ownerName}</span>
-              </div>
-              <div className="apt-notes">{item.aptNotes}</div>
+            <div className="owner-name">
+              <span className="label-item">Owner: </span>
+              <span
+                contentEditable
+                suppressContentEditableWarning
+                onBlur={(e) =>
+                  props.updateInfo("ownerName", e.target.innerText, item.aptId)
+                }
+              >
+                {item.ownerName}
+              </span>
+            </div>
+            <div
+              className="apt-notes"
+              contentEditable
+              suppressContentEditableWarning
+              onBlur={(e) =>
+                props.updateInfo("aptNotes", e.target.innerText, item.aptId)
+              }
+            >
+              {item.aptNotes}
             </div>
           </div>
-        ))}
-      </div>
-    )
+        </div>
+      ))}
+    </div>
+  );
 }
